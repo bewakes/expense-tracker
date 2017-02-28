0fd1eb8b8e0c9f1ef9affcf572b73963dabcc839
Bibek Pandey <bibek@janakitech.com>
2017-02-28 11:52:30 +0545

    update hook

--- domestic/hook.py
+++ domestic/hook.py
@@ -12,7 +12,7 @@ def webhook(request):
     execute the command here
     """
     print('webhook request')
-    p = Popen(['sh', settings.WEBHOOK_SCRIPT_PATH, settings.PASSWORD], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    p = Popen(['bash', settings.WEBHOOK_SCRIPT_PATH, settings.PASSWORD], stdin=PIPE, stdout=PIPE, stderr=PIPE)
     output, err = p.communicate(b"input data that is passed to subprocess' stdin")
     rc = p.returncode
     if not rc:
