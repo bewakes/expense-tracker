abffe8eaeac7cfaa3d81867dddb35b67114df7b5
Bibek Pandey <bibek@janakitech.com>
2017-03-14 10:09:47 +0545

    expenses update

--- assets/app/controllers/expenses.controller.js
+++ assets/app/controllers/expenses.controller.js
@@ -7,9 +7,7 @@ define(['app/app', 'services', 'directives'], function(app) {
         $scope.newExpense = {date:new Date(), description:''};
 
         $scope.reload = function() {
-            getService($scope, '/expense/', {organization:appState.current_organization.id}, 'expenses').then(function(){
-                alert(JSON.stringify($scope.expenses));
-            })
+            getService($scope, '/expense/', {organization:appState.current_organization.id}, 'expenses');
             //getService($scope, '/items/', {}, 'items');
             getService($scope, '/categories/', {}, 'categories');
             $scope.newExpense = {date:new Date(), description:''};
@@ -20,7 +18,7 @@ define(['app/app', 'services', 'directives'], function(app) {
             var temp = angular.copy($scope.expenses.filter(function(e){ return e.id == id;})[0]);
             temp.date = new Date(temp.date);
             $scope.newExpense = temp;
-            $scope.newExpense.item = temp.item.toString();
+            $scope.newExpense.category = temp.category.toString();
             if(window.innerWidth < 900){
                 $location.hash('editheader');
                 $anchorScroll();
@@ -42,7 +40,7 @@ define(['app/app', 'services', 'directives'], function(app) {
             $scope.reload();
         }
 
-        $scope.addExpense= function() {
+        $scope.addExpense = function() {
             var t, msg;
             if($scope.editMode) {
                 t = putService('/expense/'+$scope.newExpense.id+'/?organization='+appState.current_organization.id.toString(), $scope.newExpense);
