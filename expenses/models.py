f7af451b87b47a41333ca07639485fe84a04eea9
Bibekpandey <spirit_bibek@yahoo.com>
2017-03-17 22:43:52 +0000

    nothing much

--- expenses/models.py
+++ expenses/models.py
@@ -33,7 +33,7 @@ class Category(models.Model):
     description = models.CharField(max_length=1000,blank=True)
 
     def __str__(self):
-        return self.name
+        return '{} - {}'.format(self.name, self.organization.name)
 
     def delete(self):
         self.is_deleted = True
