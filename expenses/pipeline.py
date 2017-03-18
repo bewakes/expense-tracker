e1c381f34324998573fac5c3b6e72874910ac003
Bibekpandey <spirit_bibek@yahoo.com>
2017-03-18 09:15:38 +0000

    default cats for new users

--- expenses/pipeline.py
+++ expenses/pipeline.py
@@ -1,6 +1,8 @@
 import urllib.request
-from expenses.models import Organization, AppUser
+from expenses.models import Organization, AppUser, Category
 from requests import request
+
+DEFAULT_CATEGORIES = ['Groccery', 'Stationery', 'Lunch', 'Transportation']
 
 def create_org(strategy, backend, user, response, details,
 is_new=False, *args, **kwargs):
@@ -10,3 +12,11 @@ is_new=False, *args, **kwargs):
         org = Organization.objects.create(owner=user, name=user.first_name+" "+user.last_name)
         user.organizations.add(org)
         user.save()
+
+        # add categories for user/org
+        try:
+            for cat in DEFAULT_CATEGORIES:
+                category = Category.objects.create(organization=org, name=cat)
+        except Exception as e:
+            print(e)
+            pass
