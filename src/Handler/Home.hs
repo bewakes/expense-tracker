a8f08c71846ce06e32722ecff908a5a564f1e7d0
bewakes <bewakepandey@gmail.com>
2021-07-08 12:18:22 +0545

    Month wise filter

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -3,6 +3,8 @@
 module Handler.Home where
 
 import qualified Data.Text                            as T
+import           Data.Time.Calendar
+import           Data.Time.Clock
 import           Database.Esqueleto                   ((^.))
 import qualified Database.Esqueleto                   as E
 import qualified Database.Esqueleto.Internal.Internal as E
@@ -11,11 +13,6 @@ import qualified Prelude                              as P
 import           Text.Read
 import           Utils
 
--- Define our data that will be used for creating the form.
-data FileForm = FileForm
-    { fileInfo        :: FileInfo
-    , fileDescription :: Text
-    }
 
 -- This is a handler function for the GET request method on the HomeR
 -- resource pattern. All of your resource patterns are defined in
@@ -24,11 +21,7 @@ data FileForm = FileForm
 -- The majority of the code you will write in Yesod lives in these handler
 -- functions. You can spread them across multiple files if you are so
 -- inclined, or create a single monolithic file.
-
-fst3 :: (a, b, c) -> a
-fst3 (a, _, _) = a
-
-data YearMonth = YearMonth Int Int
+--
 
 getHomeR :: Handler Html
 getHomeR = do
@@ -37,27 +30,36 @@ getHomeR = do
     maybeYearraw <- lookupGetParam "year"
     let monthMaybe = coerceMaybe $ readMaybe . T.unpack <$> maybeMonthraw
         yearMaybe = coerceMaybe $ readMaybe . T.unpack <$> maybeYearraw
-        ym = YearMonth <$> yearMaybe <*> monthMaybe
-    expenses <- runDB $ getAllUserExpenses uid ym
+        curr_ = mkDay <$> yearMaybe <*> monthMaybe <*> Just 1
+    currTime <- liftIO getCurrentTime
+    day <- case curr_ of
+             Nothing -> return $ utctDay currTime
+             Just x  -> return x
+    let (prevYear, prevMonth, _) = toGregorian $ addDays (-30) day
+        (nextYear, nextMonth, _) = toGregorian $ addDays 32 day
+    liftIO $ print nextYear
+    liftIO $ print nextMonth
+    expenses <- runDB $ getAllUserExpenses uid day
     let total = fmap P.sum $ sequence $ P.map fst3 expenses
     defaultLayout $ do
         case uid of
           Nothing -> do
             setTitle "Welcome To Expense Tracker!"
             $(widgetFile "welcome")
-          Just usrid -> do
+          Just _ -> do
               setTitle "Expenses Home"
               $(widgetFile "homepage")
 
 
-getAllUserExpenses :: Maybe UserId -> Maybe YearMonth -> DB [(E.Value Double, E.Value UTCTime, E.Value Text)]
+getAllUserExpenses :: Maybe UserId -> Day -> DB [(E.Value Double, E.Value UTCTime, E.Value Text)]
 getAllUserExpenses Nothing _       = return []
-getAllUserExpenses (Just uid) ym   = do
+getAllUserExpenses (Just uid) utday = do
     E.select
            $ E.from $ \(expense `E.InnerJoin` category) -> do
                 E.on $ expense ^. ExpenseCategoryId E.==. category ^. CategoryId
                 E.where_ (expense ^. ExpenseUserId E.==. E.val uid)
-                yearMonthWhere expense
+                E.where_ (month (expense ^. ExpenseDate) E.==. E.val m)
+                E.where_ (year (expense ^. ExpenseDate) E.==. E.val (fromIntegral y))
                 return
                     ( expense   ^. ExpenseAmount
                     , expense   ^. ExpenseDate
@@ -67,8 +69,4 @@ getAllUserExpenses (Just uid) ym   = do
           month ts = E.unsafeSqlExtractSubField "month" ts
           year :: E.SqlExpr (E.Value UTCTime) -> E.SqlExpr (E.Value Int)
           year ts = E.unsafeSqlExtractSubField "year" ts
-          yearMonthWhere expense = case ym of
-                             Nothing -> E.where_ (E.val (1 :: Int) E.==. E.val (1 :: Int))
-                             Just (YearMonth y m) -> do
-                                 E.where_ (month (expense ^. ExpenseDate) E.==. E.val m)
-                                 E.where_ (year (expense ^. ExpenseDate) E.==. E.val y)
+          (y, m, _) = toGregorian utday
--- src/Utils.hs
+++ src/Utils.hs
@@ -1,8 +1,26 @@
 module Utils where
 
-import           Data.Maybe
+import           Data.Fixed
+import           Data.Time
+import           Data.Time.Calendar
+import           Prelude
 
 coerceMaybe :: Maybe (Maybe a) -> Maybe a
 coerceMaybe Nothing        = Nothing
 coerceMaybe (Just Nothing) = Nothing
 coerceMaybe (Just a)       = a
+
+
+fst3 :: (a, b, c) -> a
+fst3 (a, _, _) = a
+
+mkUTCTime :: (Integer, Int, Int)
+          -> (Int, Int, Pico)
+          -> UTCTime
+mkUTCTime (year, mon, day) (hour, mn, sec) =
+    UTCTime (fromGregorian year mon day)
+       (timeOfDayToTime (TimeOfDay hour mn sec))
+
+
+mkDay :: Int -> Int -> Int -> Day
+mkDay y m d = utctDay $ mkUTCTime (fromIntegral y, m, d) (0, 0, 0)
