4b13829c3b985edea21162ec5ca3db56eca050db
bewakes <bewakepandey@gmail.com>
2021-08-25 14:24:57 +0545

    Update gitignore and build.sh
    Rename wrong target name in build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ docker pull bewakes/expense-dependencies:latest || true
 
 docker build --target dependencies --cache-from bewakes/expense-dependencies:latest -t bewakes/expense-dependencies .
 
-docker build --target app --cache-from bewakes/expense-dependencies:latest -t bewakes/expense-tracker .
+docker build --target app --cache-from bewakes/expense-tracker:latest -t bewakes/expense-tracker .
 
 docker push bewakes/expense-dependencies:latest
 docker push bewakes/expense-tracker:latest
